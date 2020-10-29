35df26db46201a000d52f24ba663b6226bf7574a
Soohan <soohancho@icloud.com>
2020-10-29 18:01:06 +0900

    Day 1
    Basic App finished.

--- app.py
+++ app.py
@@ -22,7 +22,25 @@ def translate_input():
     #make data
     return jsonify({'result': 'success', 'trans1':
     result_string1,"trans2":result_string2})
-    
+
+@app.route('/multi-trans',methods=['POST'])
+def multi_translate():
+    string=request.form['string']
+    src=request.form['src']
+    dest1=request.form['dest1']
+    dest2=request.form['dest2']
+    #1차
+    first_translation_1=translator.translate(string,src=src,dest=dest1)
+    first_translation_2=translator.translate(string,src=src,dest=dest2)
+    first_result_1= first_translation_1.text
+    first_result_2= first_translation_2.text
+    #1차
+    final_translation_1=translator.translate(first_result_1,src=dest1,dest='ko')
+    final_translation_2=translator.translate(first_result_2,src=dest2,dest='ko')
+    final_result_1= final_translation_1.text
+    final_result_2= final_translation_2.text
+    return jsonify({'result': 'success','firstResult1':first_result_1,'firstResult2':first_result_2,'finalResult1':final_result_1,'finalResult2':final_result_2})
+
 
 if __name__ == '__main__':
     app.run('localhost', port=5000, debug=True)
